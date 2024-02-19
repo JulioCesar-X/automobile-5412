91f29c5a2a92b7debb18f3717854127187cc2b0e
Júlio César Pereira <jc.imp10@gmail.com>
2024-02-19 18:53:14 +0000

    projeto atualizado

--- formManageUsers.cs
+++ formManageUsers.cs
@@ -1,22 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Automobile
 {
     public partial class formManageUsers : Form
     {
-        private EmpresaModel empresaModel;
+
         public formManageUsers()
         {
             InitializeComponent();
-
         }
 
         private void formManageUsers_Load(object sender, EventArgs e)
